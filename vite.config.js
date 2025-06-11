1b7a6b73ce16c6d7bc042a9bc856aeb610f9860e
Roshan Gautam <mr.pawittra@gmail.com>
2025-06-11 16:31:55 +0545

    added path in the config file

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Ensures relative paths in production (important for Pages)
+  build: {
+    outDir: 'dist', // Optional (this is default, but explicit is better)
+  },
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
 })
